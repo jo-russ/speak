0652b053c0843f62870e4e97ed2976af264664d1
jo-russ <joruss@o2.pl>
2019-05-11 15:11:25 +0200

    - arguments parsing reworked - added "--q" argument to suppress console messages - added "--f <file>" argument for saving audio to file

--- Program.cs
+++ Program.cs
@@ -13,82 +13,109 @@ namespace Speak
     class Program
     {
         private static string paramStartsWith = "--";
-        
-        public static void Help() {
+
+        public static void Help(ReadOnlyCollection<InstalledVoice> voices) {
             Console.WriteLine();
             Console.WriteLine("Usage: " + Environment.GetCommandLineArgs()[0] + " ["+paramStartsWith+"v <voice>] ["+paramStartsWith+"s <speed>] ["+paramStartsWith+"l <language>] Sentence to say");
             Console.WriteLine("   " + paramStartsWith + "v <voice>    - select voice to synthetise from installed list");
             Console.WriteLine("   " + paramStartsWith + "s <speed>    - range -10...10");
             Console.WriteLine("   " + paramStartsWith + "l <language> - try to select voice based on language code. If not found - fallback to default.");
-            
+            Console.WriteLine("   " + paramStartsWith + "f <file>     - write sentence as wav to file.");
+            Console.WriteLine("   " + paramStartsWith + "q            - be quiet. Must be first option specified.");
+            Console.WriteLine();
+            Console.WriteLine("Installed voices:");
+            foreach (var voice in voices) {
+                Console.WriteLine("\"" + voice.VoiceInfo.Name + "\" : "
+                    + " ("+ voice.VoiceInfo.Age
+                    + ", "+ voice.VoiceInfo.Gender
+                    + ", "+ voice.VoiceInfo.Culture +")"
+                    + voice.VoiceInfo.Description.Replace(voice.VoiceInfo.Name,""));
+            }
+
             Console.WriteLine();
         }
+
+        public static void WriteOut(bool quiet, string message) {
+            if (!quiet) {
+                Console.WriteLine(message);
+            }
+        }
+
         public static void Main(string[] args)
         {
             using (var ss = new SpeechSynthesizer()) {
-                if (args.Length == 0) { Help(); }
                 ReadOnlyCollection<InstalledVoice> voices = ss.GetInstalledVoices();
-                Console.WriteLine("Installed voices:");
-                foreach (var voice in voices) {
-                    Console.Write("\"" + voice.VoiceInfo.Name + "\" : ");
-                    Console.Write(" ("+ voice.VoiceInfo.Age +"");
-                    Console.Write(", "+ voice.VoiceInfo.Gender +"");
-                    Console.Write(", "+ voice.VoiceInfo.Culture +")");
-                    Console.Write(""+ voice.VoiceInfo.Description.Replace(voice.VoiceInfo.Name,"") +"");
-                    Console.WriteLine();
+                if (args.Length == 0) { 
+                	Help(voices); 
+                	return;
                 }
                 string sentence = "";
-                int skip = 0;
-                string nextparam = "";                
-                bool validParam = false;
-                foreach (var toSay in args) {
-
-                    // effectively case rewritten to if's
-                    if (nextparam == paramStartsWith+"v") {
-                        ss.SelectVoice(toSay.Trim());
-                        Console.WriteLine("Using voice \"" + toSay.Trim() + "\"");
-                        validParam = true;
-                    } 
-                    if (nextparam == paramStartsWith+"s") {
-                        ss.Rate = (Math.Max(-10,Math.Min(10,int.Parse(toSay.Trim()))));
-                        Console.WriteLine("Setting speech speed to " + ss.Rate);
-                        validParam = true;
-                    }
-                    if (nextparam == paramStartsWith+"l") {
-                        bool lang_selected = false;
-                        foreach(var voice in voices) {
-                            if (voice.VoiceInfo.Culture.ToString().ToUpper().StartsWith(toSay.Trim().ToUpper())) {
-                                Console.WriteLine("Setting voice \"" + voice.VoiceInfo.Name + "\" for language \"" + toSay.Trim() + "\"");
-                                ss.SelectVoice(voice.VoiceInfo.Name);
-                                lang_selected = true;
+                string nextparam = "";
+                string option = "";
+                bool quiet = false;
+                string outFile = "";
+                int argIndex = 0;
+                for (argIndex = 0; argIndex < args.Length; argIndex++) {
+                    if (args[argIndex].StartsWith(paramStartsWith)) {
+                        option = args[argIndex].Substring(2);
+                        if (argIndex < args.Length-1) {
+                            nextparam = (args[argIndex+1]).Trim();
+                        } else {
+                            nextparam = "";
+                        }
+                        switch (option) {
+                            case "q":
+                                quiet = true;
                                 break;
-                            }
+                            case "v":
+                                ss.SelectVoice(nextparam);
+                                WriteOut(quiet, "Using voice \"" + nextparam + "\"");
+                                argIndex++;
+                                break;
+                            case "f":
+                                outFile = nextparam;
+                                WriteOut(false, "Writing sentence to \"" + outFile + "\".");
+                                argIndex++;
+                                break;
+                            case "s":
+                                ss.Rate = (Math.Max(-10,Math.Min(10,int.Parse(nextparam))));
+                                WriteOut(quiet, "Setting speech speed to " + ss.Rate);
+                                argIndex++;
+                                break;
+                            case "l":
+                                bool lang_selected = false;
+                                foreach(var voice in voices) {
+                                    if (voice.VoiceInfo.Culture.ToString().ToUpper().StartsWith(nextparam.ToUpper())) {
+                                        WriteOut(quiet, "Selecting voice \"" + voice.VoiceInfo.Name + "\" for language \"" + nextparam + "\"");
+                                        ss.SelectVoice(voice.VoiceInfo.Name);
+                                        lang_selected = true;
+                                        break;
+                                    }
+                                }
+                                if (lang_selected == false) {
+                                    WriteOut(false, "No voice could be selected for language \"" + nextparam + "\"");
+                                }
+                                argIndex++;
+                                break;
+                            default:
+                                WriteOut(quiet, "Unknown option \"" + paramStartsWith + option + "\".");
+                                break;
                         }
-                        if (lang_selected == false) {
-                            Console.WriteLine("No voice could be selected for language \"" + toSay.Trim() + "\"");
-                        }
-                        validParam = true;
-                    }
-                    if ((!validParam) && (nextparam != "")) {
-                        Console.WriteLine("Unknown option \"" + nextparam + "\"");
-                    }                            
-                    nextparam = "";
-                    
-                    
-                    // manage switches
-                    if (toSay.StartsWith(paramStartsWith)) {
-                        skip = 2;
-                        nextparam = toSay;
-                    }
-                    
-                    // skip n params if it is parameter
-                    if (skip == 0) {
-                        sentence += toSay;
                     } else {
-                        skip--;
+                        sentence += " " + args[argIndex];
                     }
                 }
+
+                if (quiet) {
+                    WriteOut(quiet, "I'll be quiet");
+                }
+                if (outFile != "") {
+                    ss.SetOutputToWaveFile(outFile);
+                } else {
+                    WriteOut(quiet, "Sentence: " + sentence);
+                }
                 ss.Speak(sentence);
+
             }
 
         }
